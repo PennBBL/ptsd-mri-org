ed23acead16fdf85adaff64a45c49e169880f74e
Adon Rosen <arosen@chead.uphs.upenn.edu>
2019-06-13 15:51:15 -0400

    Standardized hudi con script, added clean and org sections, added cp projects dir to home

--- scripts/nrco/orgFiles/hudiCon.sh
+++ scripts/nrco/orgFiles/hudiCon.sh
@@ -1,27 +1,9 @@
-unset PYTHONPATH
-unalias python
-
 ## Declare static vars
 user=`whoami`
-PYTHON=/data/joy/BBL/applications/miniconda3/envs/py2k/bin/python
 SINGULARITY=/share/apps/singularity/2.5.1/bin/singularity
-SIMG=/data/joy/BBL/applications/heudiconv/heudiconv-latest.simg
-HEURISTIC=/home/${user}/projects/nrco_bids/grmpy_heuristics.py
-
-
-BIDS_DIR=/data/joy/BBL/studies/ptsd/BIDS/${scan_id}
-TMP_BIND=/home/${whoami}/${TMPDIR}
-mkdir -p ${TMP_BIND}
-cd ${TMPDIR}
-
-## Organize the dicoms
-cd /data/jux/BBL/studies/nrco/rawData/dicomDump
-for i in `find . -name "*co*" -type d | cut -f 2 -d /` ; do
-  for q in `find ../orig/subjects/${i} -name "*dcm" -type f` ; do
-    cp ${q} ./${i}/ ;
-  done ;
-done
-
+mkdir -p ~/data
+## Now copy the projects directory with all of the heudiconv 
+cp -r /data/jux/BBL/projects/ptsd-mri-org/scripts/projects ~/
 
 ## Run all of these through a loop and submit the jobs to the grid
 mkdir ~/data
@@ -30,7 +12,20 @@ for sc in $scans;  do
   subID=$(echo $sc|cut -d'/' -f9);
   echo ${ses}
   singularity=/share/apps/singularity/2.5.1/bin/singularity
-  echo "$singularity run -B /data/joy/BBL/studies/nrco/rawData/organizedRaw/:/home/${user}/data /data/joy/BBL/applications/heudiconv/heudiconv-latest.simg -d /home/${user}/data/{subject}/* -o /home/${user}/data/joy/BBL/studies/nrco/BIDS/ -f /home/${user}/projects/nrco_bids/nrco_heur.py -s ${subID}  -c dcm2niix -b --overwrite" > ~/e${subID}_singl.sh
-  #qsub -l h_vmem=4.0G,s_vmem=4.0G ~/e${subID}_singl.sh
+  echo "$singularity run -B /data/joy/BBL/studies/nrco/rawData/organizedRaw/:/home/${user}/data /data/joy/BBL/applications/heudiconv/heudiconv-latest.simg -d /home/${user}/data/{subject}/* -o /home/${user}/data/joy/BBL/studies/nrco/BIDS/ -f /home/arosen/projects/nrco_bids/nrco_heur.py -s ${subID}  -c dcm2niix -b --overwrite" > ~/e${subID}_singl.sh
+  qsub -q basic.q,all.q -l h_vmem=4.0G,s_vmem=4.0G ~/e${subID}_singl.sh
 
 done
+
+## Add in a buffer so we can finish all of the qsub jobs
+qstatRem=`qstat | wc -l`
+while [ $qstatRem -gt 0 ] ; do
+  echo "$qstatRem jobs still remaining"
+  qstatRem=`qstat | wc -l`
+  sleep 30
+done
+echo "Now Organizing and cleaning files"
+## Now sync the files into the correct location
+rsync -r /data/joy/BBL/studies/nrco/rawData/organizedRaw/joy/BBL/studies/nrco/BIDS /data/joy/BBL/studies/nrco/
+## Now clean up the wrong path
+rm -rf /data/joy/BBL/studies/nrco/rawData/organizedRaw/joy
