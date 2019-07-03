e4c807353940f0e3ba95def8b84b8ed767cb1529
Adon Rosen <arosen@chead.uphs.upenn.edu>
2019-07-03 12:18:15 -0400

    small mods for mem requests

--- scripts/ptsd/runFS/runPTSDFSJobs.sh
+++ scripts/ptsd/runFS/runPTSDFSJobs.sh
@@ -1,12 +1,10 @@
-cp /share/apps/freesurfer/6.0.0/license.txt  /home/arosen/license.txt
-
-mkdir -p /home/arosen/data
+mkdir -p ~/data
 
 for idw in `find /data/joy/BBL/studies/ptsd/BIDS/sub-DM* -mindepth 1 -maxdepth 1 | rev | cut -f 2 -d / | rev | cut -f 2 -d - | uniq` ; do
 
-  echo /share/apps/singularity/2.5.1/bin/singularity run -B /data:/home/arosen/data /data/joy/BBL/applications/bids_apps/freesurfer.simg /home/arosen/data/joy/BBL/studies/ptsd/BIDS/ /home/arosen/data/joy/BBL/studies/ptsd/processedData/freesurfer60/ participant --participant_label ${idw} --license_file /home/arosen/data/joy/BBL/studies/ptsd/processedData/freesurfer60/license.txt --skip_bids_validator  --steps cross-sectional > subjectrun${idw}.sh
+  echo /share/apps/singularity/2.5.1/bin/singularity run -B /data:/home/arosen/data /data/joy/BBL/applications/bids_apps/freesurfer.simg /home/arosen/data/joy/BBL/studies/ptsd/BIDS/  /home/arosen/data/joy/BBL/studies/ptsd/processedData/anatomical/freesurfer60/ participant --participant_label ${idw} --license_file /home/arosen/data/joy/BBL/studies/ptsd/processedData/anatomical/freesurfer60/license.txt  --skip_bids_validator > subjectrun${idw}.sh
 
-  qsub  -l h_vmem=8.0G,s_vmem=8.0G  subjectrun${idw}.sh ;
+  qsub  -q all.q,basic.q -l h_vmem=8.0G,s_vmem=8.0G  subjectrun${idw}.sh ;
 
 done
 
@@ -14,8 +12,7 @@ done
 for idw in `find /data/joy/BBL/studies/ptsd/BIDST1/sub-DM* -mindepth 1 -maxdepth 1 | rev | cut -f 2 -d / | rev | cut -f 2 -d - | uniq` ; do
 
   echo /share/apps/singularity/2.5.1/bin/singularity run -B /data:/home/arosen/data /data/joy/BBL/applications/bids_apps/freesurfer.simg /home/arosen/data/joy/BBL/studies/ptsd/BIDST1/ /home/arosen/data/joy/BBL/studies/ptsd/processedData/anatomical/freesurfer60NoAvg/ participant --participant_label ${idw} --license_file /home/arosen/data/joy/BBL/studies/ptsd/processedData/anatomical/freesurfer60/license.txt --skip_bids_validator > subjectrunFSCS${idw}.sh
-
-  qsub  -l h_vmem=8.0G,s_vmem=8.0G  subjectrunFSCS${idw}.sh ;
+  qsub  -q all.q,basic.q -l h_vmem=8.0G,s_vmem=8.0G  subjectrunFSCS${idw}.sh ;
 
 done
 
@@ -29,6 +26,6 @@ for idw in `find /data/joy/BBL/studies/ptsd/BIDS/sub-DM* -mindepth 1 -maxdepth 1
 
   echo /share/apps/singularity/2.5.1/bin/singularity run -B /data:/home/arosen/data /data/joy/BBL/applications/bids_apps/fmriprep.simg /home/arosen/data/joy/BBL/studies/ptsd/BIDS/ /home/arosen/data/joy/BBL/studies/ptsd/processedData/fmriPrep/ participant --participant_label ${idw} --skip_bids_validation --fs-license-file /home/arosen/data/joy/BBL/studies/ptsd/processedData/anatomical/freesurfer60/license.txt > s${idw}FMRIP.sh
 
-  qsub  -l h_vmem=12.0G,s_vmem=12.0G  s${idw}FMRIP.sh ;
+  #qsub  -l h_vmem=12.0G,s_vmem=12.0G  s${idw}FMRIP.sh ;
 
 done
